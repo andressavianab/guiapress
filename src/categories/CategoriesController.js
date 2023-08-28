068d0bbd11e381051f0afb9d6c16ba19c46eadcf
Andressa Viana Batista <andressavianab@gmail.com>
2023-08-28 18:01:17 -0300

    method post

--- src/categories/CategoriesController.js
+++ src/categories/CategoriesController.js
@@ -1,13 +1,26 @@
 const app = require('express');
 const router = app.Router();
-
-router.get('/categories', (req, res) => {
-    res.send('testing categorie route!');
-});
+const Category = require('./categoryModel');
+const slugify = require('slugify');
 
 router.get('/admin/categories/new', (req, res) => {
     res.render('admin/categories/new.ejs');
 });
+
+router.post('/categories/save', (req, res) => {
+    var title = req.body.title;
+    if (title != undefined) {
+        Category.create({
+            title: title, 
+            slug: slugify(title)
+        }).then(() => {
+            res.redirect('/');
+        })
+    } else {
+        res.render('./admin/categories/new')
+    }
+});
+
 
 module.exports = router;
 /*to use the route /categories in index.js or in any other file, 
