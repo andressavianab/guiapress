b9bce262e48673992799180d78c4b822cec79538
Andressa Viana Batista <andressavianab@gmail.com>
2023-08-31 16:53:18 -0300

    edit function added

--- src/categories/CategoriesController.js
+++ src/categories/CategoriesController.js
@@ -3,26 +3,39 @@ const router = app.Router();
 const Category = require('./categoryModel');
 const slugify = require('slugify');
 
+router.get("/admin/categories", (req, res) => {
+    Category.findAll().then(categories => {
+        res.render('./admin/categories/index', {categories: categories});
+    });  
+});  // function to consume the categories database from the frontend
+
+
 router.get('/admin/categories/new', (req, res) => {
     res.render('admin/categories/new.ejs');
-});
+}); 
 
 router.post('/categories/save', (req, res) => {
+
     var title = req.body.title;
+
     if (title != undefined) {
         Category.create({
             title: title, 
             slug: slugify(title)
         }).then(() => {
-            res.redirect('/');
+            res.redirect('/admin/categories');
         })
     } else {
-        res.render('./admin/categories/new')
-    }
-});
+        res.render('admin/categories/new')
+    } 
+
+}); //function to create a new category
+
 
 router.post('/categories/delete', (req, res) => {
-    var id = req.body.id
+
+    var id = req.body.id;
+
     if(id != undefined) {
         Category.destroy(
             {
@@ -33,14 +46,40 @@ router.post('/categories/delete', (req, res) => {
         ).then(res.redirect('/admin/categories'))
     } else {
         res.redirect('/admin/categories')
-    } //function to delete a categorie
-});
+    } 
+}); //route & function to delete a category
 
-router.get("/admin/categories", (req, res) => {
-    Category.findAll().then(categories => {
-        res.render('./admin/categories/index', {categories: categories})
-    });  // function to consume the categories database from the frontend
-});  
+
+router.get('/admin/categories/edit/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(isNaN(id)) {
+        res.redirect("/admin/categories");
+    };
+
+    Category.findByPk(id).then(category => {
+        if(category != undefined) {
+            res.render("./admin/categories/edit", {category: category});
+        } else {
+            res.redirect("/admin/categories");
+        }
+    });
+
+}); //route to edit a category
+
+router.post("/categories/update", (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+
+    Category.update({title: title, slug: slugify(title)}, {
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/categories");
+    }); 
+}); //route & to save the edited category
+
 
 module.exports = router;
 /* to use the route /categories in index.js or in any other file, 
