3cff05fd21f72fcac5b66eafa71a9310ae8d0e2a
Andressa Viana Batista <andressavianab@gmail.com>
2023-09-05 13:05:28 -0300

    tinymce configs

--- src/articles/ArticlesController.js
+++ src/articles/ArticlesController.js
@@ -6,7 +6,7 @@ router.get('/articles', (req, res) => {
 });
 
 router.get('/admin/articles/new', (req, res) => {
-    res.send('route to create a new article');
+    res.render('admin/articles/new');
 });
 
 module.exports = router;
