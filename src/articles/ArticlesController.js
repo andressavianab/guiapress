531694325a2b210e772eefb789c152740c823c6c
Andressa Viana Batista <andressavianab@gmail.com>
2023-09-06 14:30:23 -0300

    post method articles & timezone configs

--- src/articles/ArticlesController.js
+++ src/articles/ArticlesController.js
@@ -1,12 +1,37 @@
 const app = require('express');
 const router = app.Router();
+const Category = require('../categories/categoryModel');
+const Article = require('./articleModel');
+const slug = require('slugify');
 
-router.get('/articles', (req, res) => {
+router.get('/admin/articles', (req, res) => {
     res.send("article route");
 });
 
 router.get('/admin/articles/new', (req, res) => {
-    res.render('admin/articles/new');
+    Category.findAll().then(categories => {
+        res.render('admin/articles/new', {categories: categories});
+    })
 });
+
+router.post('/articles/save', (req, res) => {
+    const title = req.body.title
+    const body = req.body.body
+    const categoryId = req.body.categoryId
+
+    // this validation isnt working ill fix this later =D
+    if (title != undefined && body != undefined && body != null) {
+        Article.create(
+            {
+                title: title,
+                body: body,
+                CategoryId: categoryId,
+                slug: slug(title)
+            }
+        ).then(res.redirect('/admin/articles'));
+    } else {
+        res.redirect('/admin/articles');
+    }
+})
 
 module.exports = router;
