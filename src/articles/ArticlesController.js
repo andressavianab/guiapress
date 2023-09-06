f86e41483ae361f5831712dc85416e191b28a0e1
Andressa Viana Batista <andressavianab@gmail.com>
2023-09-06 16:31:43 -0300

    articles delet & update

--- src/articles/ArticlesController.js
+++ src/articles/ArticlesController.js
@@ -5,13 +5,19 @@ const Article = require('./articleModel');
 const slug = require('slugify');
 
 router.get('/admin/articles', (req, res) => {
-    res.send("article route");
+    Article.findAll({
+        include: [
+            {model: Category}
+        ]  //Including category data by table relationship (association)
+    }).then(articles => {   
+        res.render("admin/articles/index", {articles: articles}); 
+    });
 });
 
 router.get('/admin/articles/new', (req, res) => {
     Category.findAll().then(categories => {
         res.render('admin/articles/new', {categories: categories});
-    })
+    });
 });
 
 router.post('/articles/save', (req, res) => {
@@ -32,6 +38,54 @@ router.post('/articles/save', (req, res) => {
     } else {
         res.redirect('/admin/articles');
     }
+});
+
+router.post('/articles/delet', (req, res) => {
+    const id = req.body.id
+
+    Article.destroy(
+        {
+            where: {
+                id: id
+            }
+        }
+    ).then(res.redirect('/admin/articles'));
+});
+
+router.get('/articles/edit/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(isNaN(id)) {
+        res.redirect("/admin/articles");
+    };
+
+    Article.findByPk(id).then(article => {
+        if(article != undefined) {
+            Category.findAll().then(categories => {
+                res.render('./admin/articles/edit', {categories: categories, article: article})
+            })
+        } else {
+            res.redirect('/admin/article');
+        }
+    });
+
+});
+
+router.post('/articles/update', (req, res) => {
+    const title = req.body.title;
+    const body = req.body.body;
+    const id = req.body.id;
+
+    Article.update(
+        {
+            title: title,
+            body: body
+        }, {
+            where: {
+                id: id
+            }
+        }
+    ).then(res.redirect('/admin/articles'))
 })
 
 module.exports = router;
